6ab9edf64f741cc1e00919cccdb084b8c5db6afa
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2022-05-20 10:06:07 +0100

    Add Postcss support

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,19 @@
 module.exports = {
-  stories: ['../stories/**/*.stories.@(ts|tsx|js|jsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-  // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
-  typescript: {
-    check: true, // type-check stories during Storybook build
-  }
+    stories: ['../stories/**/*.stories.@(ts|tsx|js|jsx)'],
+    addons: [
+        {
+            name: '@storybook/addon-postcss',
+            options: {
+                postcssLoaderOptions: {
+                    implementation: require('postcss'),
+                },
+            },
+        },
+        '@storybook/addon-links',
+        '@storybook/addon-essentials'
+    ],
+    // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
+    typescript: {
+        check: true, // type-check stories during Storybook build
+    }
 };
